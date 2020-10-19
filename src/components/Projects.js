fc561210b722494c6306b52674e7af72e8ca2b44
OkeyDokeyYooo <873262667a@gmail.com>
2020-10-18 23:12:55 -0700

    add website link

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,18 +1,14 @@
-import React from 'react'
+import React from "react";
 
-import Card from './Card'
-import covid19Img from '../images/covid19.png'
-import wechat from '../images/wechat.png'
-import vaneat from '../images/vaneat.svg'
+import Card from "./Card";
+import covid19Img from "../images/covid19.png";
+import wechat from "../images/wechat.png";
+import vaneat from "../images/vaneat.svg";
 
 const Projects = () => {
-
     return (
-        <section
-            name="project"
-            className="project-section"
-        >
-            <div className="title" style={{color: 'black'}}>
+        <section name="project" className="project-section">
+            <div className="title" style={{ color: "black" }}>
                 Projects
             </div>
             <div className="project-container">
@@ -21,7 +17,7 @@ const Projects = () => {
                     <h5>Here are a few projects I've worked on recently.</h5>
                 </div>
                 <div className="project-inner-container">
-                    <Card 
+                    <Card
                         imgLocation={covid19Img}
                         header="COVID-19 GLOBAL UPDATE"
                         description="A Live Tracking Website about Worldwide COVID-19 Pandemic, featured with detail information and live updated news."
@@ -32,15 +28,16 @@ const Projects = () => {
                         header="Taranto E-Shop"
                         description="WeChat based E-shop for purchasing brand Taranto bags"
                     />
-                    <Card 
+                    <Card
                         imgLocation={vaneat}
                         header="VanEat"
                         description="Designed for Vancouver local restaurant guide, user can search restaurants by category, leave reviews and rates."
+                        website="http://vaneat.herokuapp.com/"
                     />
                 </div>
             </div>
         </section>
-    )
-}
+    );
+};
 
-export default Projects
+export default Projects;
